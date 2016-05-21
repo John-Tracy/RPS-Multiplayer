04634ea61467c0f46e384094cb52dff103c837dd
John Tracy <johntracy@Johns-MacBook-Pro.local>
2016-05-21 16:08:14 -0400

    receiving data correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,17 +16,62 @@ var theGame = {
 		turn: 0,
 
 		intialSet : function() {
-
+		if(dataRef.child("1").exists() == false)
 			theGame.players[0].name = $('#name-input').val().trim();
 
-			dataRef.update(
+			dataRef.set(
+
 				{1:theGame.players[0]}
+
 				);
 
 			$('#name-input').val('');
+			$('#player-start').empty();
+			$('#player-start').append('<h4>Hello '+ theGame.players[0].name +', Waiting on player two</h4>');
+			
+			$('#player-uno').empty();
+			$('#player-uno').append('<h3>' + theGame.players[0].nametoUpperCase() + '</h3>');
+
+			if(dataRef.child("1").exists()){
+
+			theGame.players[1].name = $('#name-input').val().trim();
+
+			datRef.set(
+
+				{2:theGame.players[1]}
+
+				);
+
+			$('#name-input').val('');
+			$('#player-start').empty();
+			$('#player-start').append('<h4>Hello ' + theGame.players[1].name + ', Player 1\'s turn');
+
+			$('#player-dos').empty();
+			$('#player-dos').append('<h3>' + theGame.players[1].name.toUpperCase() + '</h3>');
+			}
 
 			return false;
 		},
+
+	//	secondSet : function() {
+//
+//		if(dataRef.child(1).exists()){
+//			theGame.players[1].name = $('#name-input').val().trim();
+//
+//			datRef.set(
+//
+//				{2:theGame.players[1]}
+//
+//				);
+
+//			$('#name-input').val('');
+//			$('#player-start').empty();
+//			$('#player-start').append('<h4>Hello ' + theGame.players[1].name + ', Player 1\'s turn');
+//
+//			$('#player-dos').empty();
+//			$('#player-dos').append('<h3>' + theGame.players[1].name.toUpperCase() + '</h3>');
+//			}
+//		},
 
 
 
@@ -38,6 +83,8 @@ var theGame = {
  	var dataRef = new Firebase(fireUrl);
 
  	$('#submit-name').on('click', theGame.intialSet);
+
+
 
 
 
